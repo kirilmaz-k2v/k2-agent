84382ea40e191c0ddec05d0e67a91164c152707c
Nader Ganayem <nader.ganayem@prospera.ag>
2023-10-25 15:56:26 +0300

    update

--- src/main/java/com/k2view/agent/K2ViewAgent.java
+++ src/main/java/com/k2view/agent/K2ViewAgent.java
@@ -1,153 +1,108 @@
 package com.k2view.agent;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
+import com.k2view.agent.dispatcher.AgentDispatcher;
+import com.k2view.agent.dispatcher.AgentDispatcherHttp;
+import com.k2view.agent.postman.CloudManager;
+import com.k2view.agent.postman.Postman;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.k2view.agent.Utils.def;
+import static com.k2view.agent.Utils.env;
+import static java.lang.Integer.parseInt;
 
 /**
- * The main class for the K2ViewAgent program.
+ * The K2ViewAgent class represents an agent that reads a list of URLs from a REST API and forwards the requests to external URLs via AgentSender object.
+ * This class uses the Gson library for JSON serialization and the HttpClient class for sending HTTP requests.
  */
 public class K2ViewAgent {
 
     /**
-     * The logger used for the K2ViewAgent program.
+     * The polling interval in seconds for checking the inbox for new messages.
      */
-    private static final Logger LOGGER = LoggerFactory.getLogger(K2ViewAgent.class);
+    private final int pollingInterval;
 
     /**
-     * The polling interval in seconds for the K2ViewAgent program.
+     * The `AgentSender` instance used for sending requests and processing responses.
      */
-    private static final int pollingInterval = 60;
+    private final AgentDispatcher dispatcher;
 
     /**
-     * The queue that holds the requests to be processed by the K2ViewAgent program.
+     * The `Postman` instance used for fetching messages from the REST API.
      */
-    private static ConcurrentLinkedQueue<Map<String, Object>> requestsQueue = new ConcurrentLinkedQueue<>();
+    private final Postman postman;
+
+    private final Executor executor;
 
     /**
-     * The main method that runs the K2ViewAgent program.
-     *
-     * @param args The command line arguments for the program.
+     * An atomic boolean that determines if the `K2ViewAgent` is running.
      */
-    public static void main(String[] args) {
-        // Start the first thread that reads a list of URLs
-        Thread managerThread = new Thread(() -> {
-            while (true) {
-                List<Map<String, Object>> requests = getInboxMessages();
+    private final AtomicBoolean running = new AtomicBoolean(true);
 
-                for (Map<String, Object> req : requests) {
-                    requestsQueue.add(req);
-                    LOGGER.info("Added URL to the Queue:" + req);
+    private final CountDownLatch latch = new CountDownLatch(1);
+
+    public K2ViewAgent(Postman postman, int pollingInterval, AgentDispatcher agentSender, Executor executor) {
+        this.postman = postman;
+        this.dispatcher = agentSender;
+        this.pollingInterval = pollingInterval;
+        this.executor = executor;
+    }
+
+
+    /**
+     * Starts the agent by initializing the `agentSender`, `id`, and `since` fields,
+     * and calling the `start()` method.
+     */
+    public static void main(String[] args) throws InterruptedException {
+        AgentDispatcherHttp sender = new AgentDispatcherHttp(10_000);
+        Postman postman = new CloudManager(env("K2_MAILBOX_ID"), env("K2_MANAGER_URL"));
+        int interval = parseInt(def(env("K2_POLLING_INTERVAL"), "10"));
+        Executor executor = (Runnable r) -> new Thread(r, "MANAGER").start();
+        new K2ViewAgent(postman, interval, sender, executor).run();
+    }
+
+    /**
+     * Starts the manager thread that continuously checks for new inbox messages
+     * and sends them to the `agentSender` for processing.
+     */
+    public void run() throws InterruptedException {
+        executor.execute(() -> {
+            try {
+                List<Response> responseList = new ArrayList<>();
+                long interval = pollingInterval;
+                String lastTaskId = "";
+                while (running.get()) {
+                    Requests inboxMessages = postman.getInboxMessages(responseList, lastTaskId);
+                    if (inboxMessages != null) {
+                        interval = inboxMessages.pollInterval() > 0 ? inboxMessages.pollInterval() : pollingInterval;
+                        for (Request req : inboxMessages.requests()) {
+                            lastTaskId = req.taskId();
+                            dispatcher.send(req);
+                            Utils.logMessage("INFO", "Added URL to the Queue:" + req);
+                        }
+                    }
+                    responseList = dispatcher.receive(interval, TimeUnit.SECONDS);
+                    Utils.logMessage("INFO", responseList.toString());
                 }
-
-                try {
-                    TimeUnit.SECONDS.sleep(pollingInterval);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                latch.countDown();
             }
         });
-        managerThread.setName("MANAGER");
-        managerThread.start();
-
-        // Start the second thread that polls the queue and GETs the URLs
-        Thread workerThread = new Thread(() -> {
-          AgentSender agentSender = new AgentSender(10);
-           while (true) {
-                int requestsCounter = 0;
-                while (!requestsQueue.isEmpty()) {
-                    requestsCounter ++;
-                    Map<String, Object> req = requestsQueue.poll();
-                    String url = req.get("url").toString().trim();
-                    ///
-                    AgentSender.Request request = new AgentSender.Request(
-                            req.get("id").toString(),
-                            url,
-                            req.get("method").toString(),
-                            "",
-                            ""
-                    );
-                    try {
-                        agentSender.send(request);
-
-                    } catch (InterruptedException e) {
-                        System.err.println("Interrupted while waiting for response: " + e.getMessage());
-                    }catch (Exception e) {
-                        System.err.println("Error while sending request: " + e.getMessage());
-                    }
-                }
-                ///
-               CompletableFuture<List<AgentSender.Response>> responses = agentSender.receiveAsync(1, TimeUnit.SECONDS);
-               List<AgentSender.Response> mailResponses = null;
-               try {
-                   mailResponses = responses.get();
-                   int numberOfResponses = 0;
-                   if (! mailResponses.isEmpty()) {
-                       Map<String, Object> responseMap = new HashMap<>();
-                       for (AgentSender.Response response: mailResponses) {
-                           responseMap.put("id", (int)Double.parseDouble(response.id()));
-                           responseMap.put("body", response.body().trim());
-                           responseMap.put("status", response.code());
-                           JsonObject responseJson = new Gson().toJsonTree(responseMap).getAsJsonObject();
-                           System.out.println(responseJson);
-                           numberOfResponses++;
-                           if (numberOfResponses == requestsCounter) agentSender.close();
-
-                       }
-                   }
-
-               } catch (InterruptedException e) {
-                   throw new RuntimeException(e);
-               } catch (ExecutionException e) {
-                   throw new RuntimeException(e);
-               }
-           }
-
-        });
-        workerThread.setName("WORKER");
-        workerThread.start();
-
+        latch.await();
     }
 
     /**
-     * Gets a list of requests from the manager's inbox via REST API.
-     *
-     * @return A list of requests as a list of maps, or null if an error occurred.
+     * Stops the agent by setting the `running` flag to `false`.
      */
-    private static List<Map<String, Object>> getInboxMessages() {
-        // Replace this code with the logic to read the URLs from the REST API
-        String url = System.getenv("K2VIEW_MANAGER_URL");
-
-        LOGGER.info("MANAGER URL: " + url);
-        HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(url))
-                .build();
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            String jsonArrayString = response.body();
-            Gson gson = new Gson();
-            List<Map<String, Object>> bodyToJson = gson.fromJson(jsonArrayString, new TypeToken<List<Map<String, Object>>>() {}.getType());
-            return bodyToJson;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public void stop() {
+        running.set(false);
     }
-
-
 }
+
+
